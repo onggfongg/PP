68b058f0107af60646acd7f316cd0a0c45c0d2ed
onggfongg <37205533+onggfongg@users.noreply.github.com>
2019-04-19 01:21:57 +0700

    Update Details.js

--- src/Details/Details.js
+++ src/Details/Details.js
@@ -1,103 +1,109 @@
 import React, { Component } from "react";
 // import { ProductConsumer, ProductProvider } from "../context";
+
 import { storeProducts } from "../data";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { addToCart } from "../redux/cart_ducks";
 import "./Details.css";
-// import Dropdown from '../Dropdown/Dropdown';
-import {
-  Dropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem
-} from "reactstrap";
+import { Label, Input } from "reactstrap";
 
 class Details extends Component {
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      dropdownOpen: false
-    };
-  }
-
-  toggle() {
-    this.setState({
-      dropdownOpen: !this.state.dropdownOpen
-    });
-  }
   state = {
-    product: {}
+    product: {},
+    localCart: []
   };
-  componentWillMount() {
+
+  componentDidMount() {
     const id = this.props.match.params.id;
     const product = storeProducts.find(product => {
       return product.id === parseInt(id);
     });
-    this.setState({ product: product.detailProduct });
+    const localCart = product.detailProduct.size.map(({ size, price }) => {
+      return {
+        title: product.detailProduct.title,
+        size,
+        price,
+        isChecked: false,
+        quantity: 0
+      };
+    });
+    this.setState({ product, localCart });
   }
+
+  addToCart = () => {
+    const { localCart } = this.state;
+    const toBeAdded = localCart.filter(size => size.isChecked);
+    toBeAdded.forEach(item => delete item.isChecked);
+    this.props.addToCart(toBeAdded);
+    this.props.history.push("/my-cart");
+  };
   render() {
-    const { product } = this.state;
-    const { title, img, description, price, info, caution } = product;
+    const { product, localCart } = this.state;
+    console.log(localCart, "localCart");
+    if (!product.title) return <p> Loading </p>;
+    const { title, img, description, info, caution, size } = product;
     return (
       <div className="container py5">
-        {/*title*/}
         <div className="row">
           <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
             <h1 className="text-title">{title}</h1>
           </div>
         </div>
-        {/*end of title*/}
-        {/*product info*/}
         <div className="col-10 mx-auto col-md-6 my-3">
           <img src={img} className="img-fluid" alt="product" />
         </div>
-        {/* <Dropdown/> */}
         <p className="text-capitalize font-weight-bold mt-3 mb-0">
           Information about product :
         </p>
         <p className="text-muted lead">{info}</p>
-        <p className="caution" >{caution}</p>
-        <h4 className="text-blue">
-          <strong>
-            price : {price} <span>Bath/kg</span>
-          </strong>
-        </h4>
-        {/* <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-        <DropdownToggle caret>
-          Dropdown
-        </DropdownToggle>
-        <DropdownMenu>
-          <DropdownItem header>Header</DropdownItem>
-          <DropdownItem disabled>Action</DropdownItem>
-          <DropdownItem>Another Action</DropdownItem>
-          <DropdownItem divider />
-          <DropdownItem>Another Action</DropdownItem>
-        </DropdownMenu>
-      </Dropdown> */}
-        ;{/*button*/}
+        <p className="caution">{caution}</p>
+        {/* <strong>
+          price : {price} <span>Bath/kg</span>
+        </strong> */}
         <div>
+          <div className="checking">
+            {localCart.map(({ size, price, isChecked }, idx) => (
+              <div key={size + idx}>
+                <Input
+                  type="checkbox"
+                  value={size}
+                  checked={isChecked}
+                  onChange={e => {
+                    let localCartClone = [...localCart];
+                    localCartClone[idx].isChecked = !localCartClone[idx]
+                      .isChecked;
+                    this.setState({
+                      localCart: localCartClone
+                    });
+                  }}
+                />
+                <p className="checking text-capitalize font-weight-bold mt-3 mb-3">
+                  {size}
+                  <span> Price: {price} Bath per Kg</span>
+                </p>
+                <input
+                  type="number"
+                  style={{ marginleft: 4, display: !isChecked && "none" }}
+                  placeholder="Quantity"
+                  onChange={e => {
+                    let localCartClone = [...localCart];
+                    localCartClone[idx].quantity = e.target.value;
+                    this.setState({
+                      localCart: localCartClone
+                    });
+                  }}
+                />
+              </div>
+            ))}
+          </div>
           <Link to="/productList">
-            <a className="btn">Back to Product Lists</a>
+            <button className="btn">Back to Product Lists</button>
           </Link>
-
-          <button
-            className="btn-addToCart"
-            onClick={() => this.props.addToCart(this.state.product)}
-          >
-            {" "}
-            Add to cart{" "}
+          <button className="btn-addToCart" onClick={() => this.addToCart()}>
+            Add to cart
           </button>
         </div>
-        {/* <h1>{count}</h1>
-
-        <h1>{total}</h1> */}
-        {/* <button onClick={() => this.props.addToCart(this.state.product)}>
-          {" "}
-          Add{" "}
-        </button> */}
       </div>
     );
   }
